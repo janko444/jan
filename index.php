183d3d59b6c1b7200cf5c8fcf54e47ffdc02ef24
janko444 <ncall5502@gmail.com>
2024-09-28 21:23:59 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
-<?php
-$file = 'visit_count.txt';
-if (file_exists($file)) {
-    $visits = (int)file_get_contents($file);
-    $visits++;
-    file_put_contents($file, $visits);
-} else {
-    file_put_contents($file, 1);
-}
-?>
+<div class="container my-5">
+    <h1>Ostatnie Artykuły</h1>
+    <div class="row">
+        <?php foreach ($articles as $article): ?>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <?php if ($article['image']): ?>
+                <img src="<?= htmlspecialchars($article['image']) ?>" class="card-img-top" alt="Obrazek">
+                <?php endif; ?>
+                <div class="card-body">
+                    <h5 class="card-title"><?= htmlspecialchars($article['title']) ?></h5>
+                    <p class="card-text"><?= substr(htmlspecialchars($article['content']), 0, 100) ?>...</p>
+                    <a href="nowosci.html" class="btn btn-primary">Czytaj więcej</a>
+                </div>
+            </div>
+        </div>
+        <?php endforeach; ?>
+    </div>
+</div>
